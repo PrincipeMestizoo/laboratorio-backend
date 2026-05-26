ca6842d40084c9db3fe431fc420c935ed7573f77
Daniel G <danielgonzalezsan4567@outlook.com>
2026-05-26 10:43:26 -0500

    first commit: init project

---
+++ laboratorio/src/test/java/com/sena/crud/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.crud.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
